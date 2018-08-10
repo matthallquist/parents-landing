feeed6bdf8f00fa91153b6c4d6f56afbb94469d1
Matt Hallquist <matthallquist@Matts-MBP.odonnellpdc.com>
2018-08-10 08:13:57 -0400

    Page completed

---
+++ themes/blankslate/page-parents.php
@@ -0,0 +1,126 @@
+<?php get_header(); ?>
+
+<div class="main-content">
+	<div class="top-links">
+		<img src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/OEC-Licns-Web-Parents-03.png">
+		<span>THE OFFICE OF EARLY CHILDHOOD</span>
+	</div>
+	<div class="hero-container">
+		<img src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/OEC-Licns-Web-Parents-04.png">
+	</div>
+	<div class="row">
+		<div class="col-lg-4 first">
+			<h2>Choosing Child Care</h2>
+			<p>
+				<strong>Check License Status</strong><br>
+				Be sure to check and see if a child care provider is
+				licensed before choosing them. You can search
+				licensed child care providers across Connecticut
+				at <strong>www.211childcare.org</strong> or by dialing <strong>211</strong>.
+			</p>
+			<p>
+				<strong>Make Priorities</strong><br>
+				Choosing the right child care for your family starts
+				 with determining your priorities. What is most
+				 important to you? Location or hours? Small or
+				 large? Does your child have special healthcare
+				 needs? Write down your needs and priorities to
+				 help think through what’s most important.
+				</p>
+			<p>
+				<strong>Determine Schedule</strong><br>
+				Consider timing: Are you looking for certain days,
+				hours, or weekend time to be covered? Do you
+				need temporary or permanent child care support?
+			</p>
+			<p>
+				<strong>Research</strong><br>
+				Do your homework by looking up potential
+				caregivers to learn more about how they operate,
+				what other families are saying about them, and to
+				get a feel for their approach. 
+			</p>
+			<p>
+				<strong>Visit & Ask Questions</strong><br>
+				Once you have selected a few potential child care
+				options, visit the locations—and bring your child!
+				Look around to see if you like the environment,
+				and talk to teachers and care providers.
+				Remember to stay involved and visit routinely
+				even after you have made your selection and
+				placed your child with a child care provider.
+			</p>			
+		</div>
+		<div class="col-lg-4 second">		
+			<h2>Types of Child Care</h2>
+			<p>
+				<strong>Family Child Care Homes</strong><br>
+				Family Child Care Providers are people who are licensed
+				to care for children in a home. Always check license
+				status when selecting your child care provider, as
+				licensed providers are held to health and safety
+				standards.
+			</p>
+			<p>
+				<strong>Group Child Care Homes</strong><br>
+				Group child care homes provide child care to 7-12
+				children on a regular basis.
+			</p>
+			<img class="whole-img" src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/OEC-Licns-Web-Parents-02.png">
+			<p>
+				<strong>Child Care Centers</strong><br>
+				A facility that offers full/part time care including nursery
+				schools, and before and after school programs to more
+				than 12 children.
+			</p>
+			<p>
+				<strong>Summer Camps</strong><br>
+				These are programs that care for children during school
+				breaks including summer.
+			</p>
+			<p>
+				<strong>Exemptions</strong><br>
+				Some child care programs are legally exempt from child
+				care licensing. Categories of exempt programs can be
+				found in Connecticut General Statutes Section
+				19a-77(b).
+			</p>
+		</div>
+		<div class="col-lg-4 third">
+			<img class="whole-img" src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/OEC-Licns-Web-Parents-05.png">
+			<a id="img-link-1" href="http://search.211childcare.org" style="height: 40px; width: 315px; position: absolute; top: 97px; right: 60px;"></a>
+			<a id="img-link-2" href="tel:211" style="height: 40px; width: 315px; position: absolute; top: 137px; right: 60px;"></a>
+			<h2>Why a License is Important</h2>
+			<p>
+				<strong>Training & Background Checks </strong><br>
+				Licensed providers receive background checks and
+				medical exams for all staff members and staff are trained
+				in ﬁrst aid.
+			</p>
+			<p>
+				<strong>Safety Standards</strong><br>
+				Licensed providers must meet standards for cleanliness,
+				 freedom of hazards, and ﬁre safety.
+			</p>
+			<p>
+				<strong>Quality Care</strong><br>
+				Licensed providers only care for a limited amount of
+				children at one time, and all staff meet standards
+				monitored by the OEC Licensing Division.
+			</p>
+			<p>
+				<strong>Careful Oversight</strong><br>
+				Licensed providers agree to unscheduled health and
+				safety inspections from state licensing staff.
+			</p>
+			<p>
+				<strong>Support System</strong><br>
+				Concerned parents can ﬁle a complaint with OEC, who
+				investigate the claim and ensure that health and safety
+				standards are always upheld.
+			</p>
+		</div>				
+	</div>
+</div>
+
+<?php get_footer(); ?>
