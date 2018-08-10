b90a252d91ba70ac83cdd39604901d593cc11d59
Matt Hallquist <matthallquist@Matts-MBP.odonnellpdc.com>
2018-08-10 10:47:57 -0400

    'license' page built and responsive

--- themes/blankslate/page-license.php
+++ themes/blankslate/page-license.php
@@ -2,6 +2,89 @@
 
 <div class="main-content">
 	<?php get_template_part('partial', 'header') ?>
+	<div class="row">
+		<div class="col-lg-4 col-md-5 col-sm-5 first">
+			<img class="full" src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/linked-img-license_01.jpg">
+			<a href="http://www.ct.gov/oec/lib/oec/licensing/childcare/family_initial_app.pdf"><img class="full" src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/linked-img-license_02.jpg"></a>
+			<a href="tel:8002826063"><img class="full" src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/linked-img-license_03.jpg"></a>
+			<h2>Benefits of Your License</h2>
+			<div class="row dash-border img-stack">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<img src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/OEC-Licns-Web-Home-Day-Care-02-1.png">
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<span>A child care license provides added support and structure for your home business, such as:</span>
+				</div>
+			</div>
+			<div class="row dash-border img-stack">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<img src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/OEC-Licns-Web-Home-Day-Care-07-1.png">
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<span>Resources that support you in providing child care in your home.</span>
+				</div>
+			</div>
+			<div class="row dash-border img-stack">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<img src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/OEC-Licns-Web-Home-Day-Care-08-1.png">
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<span>Access to early childhood professionals who provide guidance and support.</span>
+				</div>
+			</div>
+			<div class="row dash-border img-stack">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<img src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/OEC-Licns-Web-Home-Day-Care-09-1.png">
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<span>Support with health and safety standards to keep children safe in your care.</span>
+				</div>
+			</div>
+			<div class="row img-stack">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<img src="http://localhost:8888/ctchild/wp-content/uploads/2018/08/OEC-Licns-Web-Home-Day-Care-10-1.png">
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<span>Ability to advertise services and show that you uphold health and safety standards.</span>
+				</div>
+			</div>									
+		</div>
+		<div class="col-lg-8 col-md-7 col-sm-7">
+			<h2>How to Start Your Home Child Care Program</h2>
+			<p>
+				<strong>
+					Child care is an important part of every community. If you are 20 years or older, love working with children, and feel you are good with children, then opening a home based, family daycare may be the business for you. You can make a difference for families and children every day, working right from your own home! Below are some key steps to get your business off the ground.
+				</strong>
+			</p>
+			<h6><strong>STEP ONE</strong></h6>
+			<p>				
+				The first step in starting your home daycare is to apply for your license. You can download the
+				application, or call 800-282-6063 to have it mailed. There are several steps to the application
+				process, including background checks for all those in the home, as well as medical exams. For a full
+				list of application steps, download the application packet.
+			</p>
+			<h6><strong>STEP TWO</strong></h6>
+			<p>
+				After your application is processed, you will receive guidance from the OEC to help ensure your
+				home is in compliance with all health and safety requirements. For more information on home
+				inspections, call 1-800-282-6063.
+			</p>
+			<h6><strong>STEP THREE</strong></h6>
+			<p>
+				Develop your business concept, insurance needs, and business type. Take the time to consider how
+				your business will operate. How much will you charge? Will you close for all holidays? What will your
+				expenses be? What types of activities do you want to offer? Look at insurance requirements for
+				running a business from your home, and consider your budget as well as the structure of your
+				business. Dial 211 for insurance information.
+			</p>
+			<h6><strong>STEP FOUR</strong></h6>
+			<p>
+				Once you have your business figured out, and your license is issued, it’s time to promote your new
+				business! Spread the word with neighbors, friends, and on social media. In no time, your home will be
+				filled with little ones ready to learn and play!				
+			</p>
+		</div>
+	</div>
 </div>
 
 <?php get_footer(); ?>
